a4d22491f28703102c2e976b62731f03a4ddbe6a
Tobiloba Ogundiyan <apprentice_py@icloud.com>
2025-11-10 21:38:19 -0500

    added code to boostrap s3 state

--- bootstrap/terraform.tf
+++ bootstrap/terraform.tf
@@ -1,11 +1,15 @@
 terraform {
+  required_version = ">= 1.13.0, < 1.14.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.92"
+      version = "~> 6.0"
     }
   }
-  required_version = ">= 1.2"
 }
 
+# configure provider
+provider "aws" {
+  region = var.region
+}
 
