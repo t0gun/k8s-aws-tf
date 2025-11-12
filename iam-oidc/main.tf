c45e46d339a2b42519e7d1699b312a5e1eae4120
Tobiloba Ogundiyan <apprentice_py@icloud.com>
2025-11-12 00:17:40 -0500

    completed security-provisioning

--- iam-oidc/main.tf
+++ iam-oidc/main.tf
@@ -87,6 +87,55 @@ resource "aws_iam_role_policy_attachment"  "attach_state-access" {
   policy_arn = aws_iam_policy.state_access.arn
 }
 
+###### Permissions needed for real infra starts Here
+data "aws_caller_identity"  "me" {}  # we use it for access to account id
+
+# Names  I will use in prod code later
+locals {
+  project = "k8s-aws-tf"
+  ec2_role_name = "k8s-ec2-ssm-role"
+  ec2_profile_name = "k8s-ec2-ssm-profile"
+  ec2_role_arn = "arn:aws:iam::${data.aws_caller_identity.me.account_id}:role/${local.ec2_role_name}"
+  ec2_profile_arn = "arn:aws:iam::${data.aws_caller_identity.me.account_id}:instance-profile/${local.ec2_profile_name}"
+}
+
+
+# full infra polic . we loosen ec2 to prevent quirks and tighten its IAM
+resource "aws_iam_policy" "tf_apply_for_k8s_aws" {
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {"Effect":"Allow", "Action":["sts:GetCallerIdentity"], "Resource":"*" },
+      {"Effect": "Allow", "Action":["ec2:*"], "Resource":"*" },
+      # IAM needed to create the EC2 SSM/Role profile
+      {
+        "Effect":"Allow",
+        "Action": [
+          "iam:CreateRole","iam:DeleteRole","iam:GetRole","iam:TagRole","iam:UntagRole",
+          "iam:AttachRolePolicy","iam:DetachRolePolicy",
+          "iam:CreateInstanceProfile","iam:DeleteInstanceProfile",
+          "iam:AddRoleToInstanceProfile","iam:RemoveRoleFromInstanceProfile",
+          "iam:ListAttachedRolePolicies","iam:GetInstanceProfile","iam:TagInstanceProfile","iam:UntagInstanceProfile"
+        ],
+        "Resource":[ "${local.ec2_role_arn}", "${local.ec2_profile_arn}"]
+      },
+
+      # PassRole only for the EC2 instance role, and only to the EC2 service
+      {
+        "Effect":"Allow",
+        "Action":"iam:PassRole",
+        "Resource": "${local.ec2_role_arn}",
+        "Condition": { "StringEquals": { "iam:PassedToService": "ec2.amazonaws.com" } }
+      },
+    ]
+
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_infra_permissions_to_oidc_role" {
+  role       = aws_iam_role.k8-aws-tf-apply.name
+  policy_arn = aws_iam_policy.tf_apply_for_k8s_aws.arn
+}
 
 
 output "gha_role_arn" {
