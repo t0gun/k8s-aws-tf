9292c198bd6afe12051eab2de7f61324f00ca5d7
Tobiloba Ogundiyan <apprentice_py@icloud.com>
2025-11-25 18:47:38 -0500

    added public subnet

--- infra/network.tf
+++ infra/network.tf
@@ -10,6 +10,19 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "k8-aws-tf-public-subnet"
+    ManagedBy = "terraform"
+  }
+}
+
+
 resource "aws_subnet" "private" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
