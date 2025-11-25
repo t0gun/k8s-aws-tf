3f7fb51ca94c121901a5f1fa66d9144c474a8724
Tobiloba Ogundiyan <apprentice_py@icloud.com>
2025-11-24 19:56:26 -0500

    added variables for compute

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,15 @@ variable "region" {
   type = string
   default = "ca-central-1"
 }
+
+variable "instance_type" {
+  description = "EC2 instance type for all KTHW nodes"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "root_volume_size_gb" {
+  description = "Root volume size for KTHW nodes"
+  type        = number
+  default     = 20
+}
