fbe810fa68839adb837833df71c9edf289f7a9f1
Tobiloba Ogundiyan <apprentice_py@icloud.com>
2025-11-26 20:53:59 -0500

    added tf outputs as github artifacts

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -13,3 +13,12 @@ output "kthw_private_ips" {
     node_1 = aws_instance.node_1.private_ip
   }
 }
+
+output "machines_txt" {
+  description = "Ready-to-paste machines.txt for KTHW"
+  value = <<EOT
+${aws_instance.server.private_ip}  server.kubernetes.local  server  10.200.0.0/24
+${aws_instance.node_0.private_ip} node-0.kubernetes.local  node-0  10.200.1.0/24
+${aws_instance.node_1.private_ip} node-1.kubernetes.local  node-1  10.200.2.0/24
+EOT
+}
